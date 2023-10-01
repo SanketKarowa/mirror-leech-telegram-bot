807137ca9a2850de2b2e0fd9dc5ce52988cfe0fe
Sachin Oraon <sachinoraon55@gmail.com>
2023-10-01 14:57:05 +0530

    fix path for ytdl

--- bot/helper/listeners/task_listener.py
+++ bot/helper/listeners/task_listener.py
@@ -350,7 +350,7 @@ class MirrorLeechListener:
                 mime_type = get_mime_type(file_path)
             else:
                 mime_type = "Folder"
-            new_path = f"{DOWNLOAD_DIR}" + "_".join(''.join(letter for letter in f"{up_name}" if letter.isalnum() or letter in [' ', '.', '-', '_']).split())
+            new_path = f"{ospath.dirname(self.dir)}/" + "_".join(''.join(letter for letter in f"{up_name}" if letter.isalnum() or letter in [' ', '.', '-', '_']).split())
             LOGGER.info(f"Renaming {up_dir} to {new_path}")
             await rename(self.dir, new_path)
             self.dir = new_path
